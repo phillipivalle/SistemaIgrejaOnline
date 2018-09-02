27ad274e1b54613d9a71dc49d902ffa7bd31ff88
Clóvis Fernando Gomes <cfogomes@hotmail.com>
2018-09-02 20:46:36 -0300

    Tela Culto

--- IgrejaOnline/IgrejaOnline/Views/WPFListHorarios.xaml.cs
+++ IgrejaOnline/IgrejaOnline/Views/WPFListHorarios.xaml.cs
@@ -23,5 +23,10 @@ namespace IgrejaOnline.Views
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
