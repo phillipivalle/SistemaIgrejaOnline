0fbbea5f0ecc493968e0a6efa0185cfb00dfd90d
phillipivalle <41653782+phillipivalle@users.noreply.github.com>
2018-09-13 21:44:45 -0300

    Controller Dizimista

---
+++ IgrejaOnline/Controllers/DizimistaController.cs
@@ -0,0 +1,76 @@
+﻿using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    class DizimistaController
+    {
+        void inserirDizimista(Dizimistas d)
+        {
+            IgrejaBDContainer contexto = new IgrejaBDContainer();
+            contexto.DizimistasSet.Add(d);
+            contexto.SaveChanges();
+        }
+
+
+        List<Dizimistas> ListarTodosDizimistas()
+        {
+            IgrejaBDContainer contexto = new IgrejaBDContainer();
+            return contexto.DizimistasSet.ToList();
+        }
+
+        Dizimistas BuscarPorNome(string nome)
+        {
+            IgrejaBDContainer contexto = new IgrejaBDContainer();
+            return contexto.DizimistasSet.Find(nome);
+        }
+
+        void Excluir(string nome)
+        {
+            Dizimistas dExcluir = BuscarPorNome(nome);
+
+            if (dExcluir != null)
+            {
+                IgrejaBDContainer contexto = new IgrejaBDContainer();
+                contexto.DizimistasSet.Remove(dExcluir);
+                contexto.SaveChanges();
+            }
+        }
+
+        void Editar(string nome, Dizimistas NovosDadosDizimista)
+        {
+            Dizimistas DizimistaAntigo = BuscarPorNome(nome);
+
+            if (DizimistaAntigo != null)
+            {
+
+                DizimistaAntigo.Nome = NovosDadosDizimista.Nome;
+                DizimistaAntigo.CPF = NovosDadosDizimista.CPF;
+                DizimistaAntigo.Sexo = NovosDadosDizimista.Sexo;
+                DizimistaAntigo.DataNasci = NovosDadosDizimista.DataNasci;
+                DizimistaAntigo.Endereco = NovosDadosDizimista.Endereco;
+                DizimistaAntigo.Numero = NovosDadosDizimista.Numero;
+                DizimistaAntigo.Bairro = NovosDadosDizimista.Bairro;
+                DizimistaAntigo.CEP = NovosDadosDizimista.CEP;
+                DizimistaAntigo.Cidade = NovosDadosDizimista.Cidade;
+                DizimistaAntigo.UF = NovosDadosDizimista.UF;
+                DizimistaAntigo.NCartao = NovosDadosDizimista.NCartao;
+                DizimistaAntigo.Validade = NovosDadosDizimista.Validade;
+                DizimistaAntigo.CodSeguranca = NovosDadosDizimista.CodSeguranca;
+                DizimistaAntigo.Salario = NovosDadosDizimista.Salario;
+                DizimistaAntigo.NomeImpresso = NovosDadosDizimista.NomeImpresso;
+                DizimistaAntigo.Bandeira = NovosDadosDizimista.Bandeira;
+
+                IgrejaBDContainer contexto = new IgrejaBDContainer();
+
+                contexto.Entry(DizimistaAntigo).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+    }
+
+}
