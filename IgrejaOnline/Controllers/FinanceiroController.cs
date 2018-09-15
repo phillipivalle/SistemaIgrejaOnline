308dd5c8ca37010c094a3340fe155bbaa0dc575d
Clóvis Fernando Gomes <cfogomes@hotmail.com>
2018-09-15 19:35:01 -0300

    Financeiro Controller

---
+++ IgrejaOnline/Controllers/FinanceiroController.cs
@@ -0,0 +1,47 @@
+﻿using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    class FinanceiroController
+    {
+        void inserir(Financeiro g)
+        {
+            IgrejaBDContainer contexto = new IgrejaBDContainer();
+            contexto.DizimistasSet.Add(g);
+            contexto.SaveChanges();
+        }
+
+
+        List<Financeiro> ListarTodoFinanceiro()
+        {
+            IgrejaBDContainer contexto = new IgrejaBDContainer();
+            return contexto.FinanceiroSet.ToList();
+        }
+
+        Financeiro BuscarPorNome(string nome)
+        {
+            IgrejaBDContainer contexto = new IgrejaBDContainer();
+            return contexto.FinanceiroSet.Find(nome);
+        }
+
+        void Excluir(string nome)
+        {
+            Financeiro dExcluir = BuscarPorNome(nome);
+
+            if (dExcluir != null)
+            {
+                IgrejaBDContainer contexto = new IgrejaBDContainer();
+                contexto.DizimistasSet.Remove(dExcluir);
+                contexto.SaveChanges();
+            }
+        }
+
+
+
+    }
+}
